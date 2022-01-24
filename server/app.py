e8d1032ac02e6179a486903140970fae3b999714
cyberjabka <76742792+sichiiii@users.noreply.github.com>
2022-01-25 02:48:28 +0300

    host fix

--- server/app.py
+++ server/app.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.secret_key = 'some_secret'
     
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
